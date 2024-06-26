771605c78dcd7b177824666666112b42e37518a0
Rachel McMahan <rae.mcmahan@gmail.com>
2024-06-26 00:16:03 -0700

    Moved logic for what each route does to a controller file. Seems to making this more MVC-like.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,8 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {getProducts, getProductById} from "../controllers/productController.js";
 
-// Get all products
-router.get(
-    "/",
-    asyncHandler(async (req, res) => {
-        const products = await Product.find({});
-        res.json(products);
-    })
-);
-
-// Get product by ID
-router.get(
-    "/:id",
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404);
-            throw new Error(`Product ID ${req.params.id} not found.`);
-        }
-    })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
