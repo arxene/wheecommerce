bc798a3f4f0beb5a86b24a0217bbbd3e70a490d1
Rachel McMahan <rae.mcmahan@gmail.com>
2024-06-25 18:19:50 -0700

    Added dotenv package to be able to use environment variables in .envv for things like port number.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+
 import products from "./data/Products.js";
-const port = 5000;
 
 const app = express();
 
