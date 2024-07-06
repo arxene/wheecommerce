6cd298f754adb6a53b32c88f02f781aaf22aa4fb
Rachel McMahan <rae.mcmahan@gmail.com>
2024-07-05 19:19:35 -0700

    Implement log out by clearing the jwt cookie.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send("logout user");
+    // clear the JWT login cookie
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(StatusCodes.OK).json({message: "Logged out successfully"});
 });
 
 // @desc    Get user profile
