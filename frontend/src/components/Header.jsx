3d320aaaad65663a260fc1c6ab53607b125db04b
Rachel McMahan <rae.mcmahan@gmail.com>
2024-07-08 15:22:10 -0700

    Implement the Sign In functionality. When signed in, clicking the username in the header shows links to Profile and Sign Out which are not yet implemented.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import {Badge, Nav, Navbar, Container} from "react-bootstrap";
+import {Badge, Nav, Navbar, Container, NavDropdown} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
 import {LinkContainer} from "react-router-bootstrap";
 import {useSelector} from "react-redux";
@@ -6,6 +6,11 @@ import logo from "../assets/wc-high-resolution-logo-white-transparent.png";
 
 const Header = () => {
     const {cartItems} = useSelector((state) => state.cart); // the name cart comes from store.js
+    const {userInfo} = useSelector((state) => state.auth);
+
+    const logoutHandler = () => {
+        console.log("logging out");
+    };
 
     return (
         <header>
@@ -32,11 +37,20 @@ const Header = () => {
                                 </Nav.Link>
                             </LinkContainer>
 
-                            <LinkContainer to="/login">
-                                <Nav.Link>
-                                    <FaUser /> Sign In
-                                </Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id="username">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Sign Out</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link>
+                                        <FaUser /> Sign In
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,15 +1,44 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import {useState, useEffect} from "react";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import {Form, Button, Row, Col} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import {useLoginMutation} from "../slices/usersApiSlice";
+import {setCredentials} from "../slices/authSlice";
+import {toast} from "react-toastify";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const submitHandler = (e) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, {isLoggingIn}] = useLoginMutation();
+
+    const {userInfo} = useSelector((state) => state.auth);
+
+    // check if we need to redirect to a different page after login
+    const {search} = useLocation();
+    const searchParams = new URLSearchParams(search);
+    const redirect = searchParams.get("redirect") || "/"; // if no ?redirect= then default to /
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [userInfo, navigate, redirect]);
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log("Attempting login...");
+
+        try {
+            const res = await login({email, password}).unwrap();
+            dispatch(setCredentials({...res})); // puts userInfo in local storage
+            navigate(redirect);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
     };
 
     return (
@@ -37,14 +66,16 @@ const LoginScreen = () => {
                     ></Form.Control>
                 </Form.Group>
 
-                <Button type="submit" variant="primary" className="mt-2">
+                <Button type="submit" variant="primary" className="mt-2" disabled={isLoggingIn}>
                     Sign In
                 </Button>
+
+                {isLoggingIn && <Loader />}
             </Form>
 
             <Row className="py-3">
                 <Col>
-                    New Customer? <Link to="/register">Register</Link>
+                    New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>Register</Link>
                 </Col>
             </Row>
         </FormContainer>
