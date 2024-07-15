4db65f975549c3f9507af6534da5f7294035ba13
Rachel McMahan <rae.mcmahan@gmail.com>
2024-07-14 18:12:32 -0700

    Add ability to update user profile (name, email, pwd).

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,7 +96,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update user profile
-// @route   PUT /api/users/profile (TODO: doesn't need :id because will use JWT token to update their profile)
+// @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
