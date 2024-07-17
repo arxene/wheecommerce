25b230667a902f54f3368326f8b8138ecab41eff
Rachel McMahan <rae.mcmahan@gmail.com>
2024-07-17 16:15:29 -0700

    Implement the product delete functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,7 +46,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
 // @desc    Get all products
 // @route   PUT /api/products/:id
-// @access  Private.Admin
+// @access  Private/Admin
 const updateProduct = asyncHandler(async (req, res) => {
     const {name, price, description, image, brand, category, countInStock} = req.body;
 
@@ -69,4 +69,19 @@ const updateProduct = asyncHandler(async (req, res) => {
     }
 });
 
-export {getProducts, getProductById, createProduct, updateProduct};
+// @desc    Delete a products
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        await Product.deleteOne({_id: product._id});
+        res.status(StatusCodes.OK).json({message: "Product deleted"});
+    } else {
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("Resource not found");
+    }
+});
+
+export {getProducts, getProductById, createProduct, updateProduct, deleteProduct};
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,19 +1,27 @@
 import {LinkContainer} from "react-router-bootstrap";
 import {Table, Button, Row, Col} from "react-bootstrap";
-import {FaTimes, FaEdit, FaTrash} from "react-icons/fa";
+import {FaEdit, FaTrash} from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import {toast} from "react-toastify";
-import {useGetProductsQuery, useCreateProductMutation} from "../../slices/productsApiSlice";
-import {PRODUCTS_URL} from "../../constants";
+import {useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation} from "../../slices/productsApiSlice";
 
 const ProductListScreen = () => {
     const {data: products, isLoading, error, refetch} = useGetProductsQuery();
 
+    const [deleteProduct, {isLoading: loadingDelete}] = useDeleteProductMutation();
+    const deleteHandler = async (id) => {
+        if (window.confirm("Are you sure you want to delete this product?")) {
+            try {
+                await deleteProduct(id);
+                refetch();
+            } catch (err) {
+                toast.error(err?.data?.message || err.error);
+            }
+        }
+    };
+
     const [createProduct, {isLoading: loadingCreate}] = useCreateProductMutation();
-
-    const deleteHandler = (id) => {};
-
     const createProductHandler = async () => {
         if (window.confirm("Are you sure you want to create a new product?")) {
             try {
@@ -40,6 +48,7 @@ const ProductListScreen = () => {
             </Row>
 
             {loadingCreate && <Loader />}
+            {loadingDelete && <Loader />}
             {isLoading ? (
                 <Loader />
             ) : error ? (
